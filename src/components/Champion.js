5b003a19f7c9216ec7053cbf50bff5d8d8ae3e64
Duy Pham <duy211099@gmail.com>
2020-12-14 18:38:55 +0700

    fixed long name Champion components

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,17 +1,23 @@
 import React from "react";
-// Styling & Anim
+// Styled & Anim
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Champion = ({ name }) => {
+const Champion = ({ name, id }) => {
   // Get logo URL
   const getLogo = (name) =>
     `http://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/${name}.png`;
 
   return (
-    <div>
-      <img src={getLogo(name)} alt="" />
-      <p>{name}</p>
-    </div>
+    <StyledChampion>
+      <img src={getLogo(id)} alt="" />
+      <p>{name.length > 12 ? name.substring(0, 8) + "..." : name}</p>
+    </StyledChampion>
   );
 };
+
+const StyledChampion = styled(motion.div)`
+  padding: 0.5rem;
+`;
 
 export default Champion;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,8 @@ import { loadChampions } from "../actions/championAction";
 // Styled & Anim
 import styled from "styled-components";
 import { motion } from "framer-motion";
+// Components
 import Champion from "../components/Champion";
-import Aatrox from "../img/img/champion/loading/Aatrox_0.jpg";
-var Diamond = require("../img/img/champion/loading/Aatrox_0.jpg");
 
 function Home() {
   const dispatch = useDispatch();
@@ -19,16 +18,25 @@ function Home() {
   const { champions } = useSelector((state) => state.champion);
 
   return (
-    <ChampionList>
+    <StyledHome>
       <h2>Tướng</h2>
-      {champions.map((champion) => (
-        <Champion name={champion.name} key={champion.id} />
-      ))}
-    </ChampionList>
+      <ChampionList>
+        {champions.map((champion) => (
+          <Champion name={champion.name} id={champion.id} key={champion.id} />
+        ))}
+      </ChampionList>
+    </StyledHome>
   );
 }
 
-const ChampionList = styled(motion.div)``;
-const Champions = styled(motion.div)``;
+const StyledHome = styled(motion.div)`
+  padding: 2rem;
+`;
+
+const ChampionList = styled(motion.div)`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2rem;
+`;
 
 export default Home;
